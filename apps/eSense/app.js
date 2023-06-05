31143bb10073c4f80dbde1f921885c39fe01f388
Daniel Karl <daniel.karl@student.uni-siegen.de>
2023-06-05 16:37:24 +0200

    write eSense data into csv file

--- apps/eSense/app.js
+++ apps/eSense/app.js
@@ -1,11 +1,21 @@
-var gatt; 
+var gatt;
 var service;
-var Layout = require("Layout");
+
+/* Create the file in append mode
+columns (1-4): cmd, packetIndex, checksum, size
+columns (5-6): gyro-x_msb, gyro-x_lsb
+columns (7-8): gyro-y_msb, gyro-y_lsb
+columns (9-10): gyro-z_msb, gyro-z_lsb
+columns (11-12): accl-x_msb, accl-x_lsb
+columns (13-14): accl-y_msb, accl-y_lsb
+columns (15-16): accl-z_msb, accl-z_lsb
+*/
+var file = require("Storage").open("eSenseData.csv","a");
 
 function connectToESense() {
     NRF.requestDevice({
       filters: [{
-        name: 'eSense-0770'
+        namePrefix: 'eSense'
       }]
     }).then(function(device) {
       return device.gatt.connect();
@@ -14,18 +24,27 @@ function connectToESense() {
       gatt = g;
       return gatt.getPrimaryService(
         "0xFF06");
-    }).then(function(service) {
-      s = service;
+    }).then(function(s) {
+      service = s;
       return service.getCharacteristic(
         "0xFF07");
     }).then(function(c) {
-      return c.writeValue([83, 33, 2, 1, 30]);
+      return c.writeValue([83, 13, 2, 1, 10]);
     }).then(function() {
-      return s.getCharacteristic("0xFF08");
-    }).then(function(s) {
-      return s.readValue();
-    }).then(function(dataview){
-      console.log(dataview.buffer);
+      return service.getCharacteristic("0xFF08");
+    }).then(function(c) {
+      counter = 0;
+      c.on('characteristicvaluechanged', function(event) {
+    //console.log("-> ",event.target.value);
+        var csv = [].slice.call(event.target.value.buffer);
+        console.log(csv);
+        file.write(csv.join(",")+"\n");
+        counter++;
+    if (counter > 20){c.stopNotifications();}
+  });
+      return c.startNotifications();
+    }).catch(function(){
+      console.log("Notification Error");
     });
   }
   
@@ -41,4 +60,3 @@ setWatch(() => {
   }, BTN1, {
     repeat: true
   });
-
