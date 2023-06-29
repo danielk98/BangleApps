1847b625374d36716b57fa2cbaef93f401a00676
phil8u <136286826+phil8u@users.noreply.github.com>
2023-06-30 01:54:43 +0200

    + created menu and split up functionality

--- apps/eSense/app.js
+++ apps/eSense/app.js
@@ -1,16 +1,21 @@
-/**
- * @type {BluetoothRemoteGATTServer}
- */
-var gatt;
+/** @type {BluetoothRemoteGATTServer} */
+let gatt;
+/** @type {BluetoothRemoteGATTService} */
+let service;
 
-/**
- * @type {BluetoothRemoteGATTService}
- */
-var service;
+/** @type {StorageFile} */
+let bangleAccel;
+/** @type {StorageFile} */
+let eSenseAccel;
+/** @type {StorageFile} */
+let eSenseGyros;
 
-let isStarted = false; //tracks first button press and switches button function
+let startedAt;
+
 let isStopped = false; //true when button is pressed a 2nd time, stops datatransmission
 
+let sampleRate = 25;
+let counter = 0;
 
 const sampleRate = 25;
 
@@ -24,87 +29,349 @@ columns (12-13): accl-y_msb, accl-y_lsb
 columns (14-15): accl-z_msb, accl-z_lsb
 */
 
-function cleanFiles() {
-  require("Storage").open("eSenseGyros.csv", "r").erase();
-  require("Storage").open("eSenseGyrosMag.csv", "r").erase();
-  require("Storage").open("eSenseAccel.csv", "r").erase();
-  require("Storage").open("eSenseAccelMag.csv", "r").erase();
-  require("Storage").open("bangleAccel.csv", "r").erase();
-  require("Storage").open("bangleAccelMag.csv", "r").erase();
-}
-
-// Create/Open the file in append mode
-//let eSenseData = require("Storage").open("eSenseData.csv","a");
-let eSenseGyros = require("Storage").open("eSenseGyros.csv","a");
-let eSenseGyrosMag = require("Storage").open("eSenseGyrosMag.csv","a");
-let eSenseAccel = require("Storage").open("eSenseAccel.csv","a");
-let eSenseAccelMag = require("Storage").open("eSenseAccelMag.csv","a");
-let bangleAccel = require("Storage").open("bangleAccel.csv", "a");
-let bangleAccelMagnitude = require("Storage").open("bangleAccelMag.csv", "a");
-
+/**
+ * @returns {Promise<BluetoothRemoteGATTServer>}
+ */
 function connectToESense() {
-  NRF.requestDevice({
+  return NRF.requestDevice({
     filters: [{
       namePrefix: 'eSense'
     }]
-  }).then(function(device) {
-    return device.gatt.connect();
-  }).then(function(g) {
-    console.log("Connected to eSense");
-    gatt = g;
-    // Get general eSense BLE service
-    return gatt.getPrimaryService("0xFF06");
-  }).then(function(s) {
-    console.log("Found primary service");
-    service = s;
-    // IMU sensor config characterisic
-    return service.getCharacteristic("0xFF07");
-  }).then(function(c) {
-    console.log("Found config character");
-    // Activate IMU sensor and set sampling rate
-    return c.writeValue([83, 28, 2, 1, 25]);
-  }).then(function() {
-    console.log("Set sampling rate");
-    // Characteristic to receive sensor data
-    return service.getCharacteristic("0xFF08");
-  }).then(function(c) {
-    console.log("Found sensor character");
-    // Set listener for eSense notifications
-    counter = 0;
-    // start Bangle Acclerometer
-    startUpAccel();
-    console.log("Starting Listener for IMU sensor data");
-    c.on('characteristicvaluechanged', function(event) {
-      let data = [].slice.call(event.target.value.buffer);
-      console.log("eSense: " + data);
-      //eSenseData.write(data.join(",")+"\n");
-      let accl_x = combineBytes(data[10], data[11]);
-      let accl_y = combineBytes(data[12], data[13]);
-      let accl_z = combineBytes(data[14], data[15]);
-      eSenseAccel.write("" + accl_x + "," + accl_y + "," + accl_z + "\n");
-      saveSensorDataMagnitude(eSenseAccelMag, accl_x, accl_y, accl_z);
-      let gyro_x = combineBytes(data[4], data[5]);
-      let gyro_y = combineBytes(data[6], data[7]);
-      let gyro_z = combineBytes(data[8], data[9]);
-      eSenseGyros.write("" + gyro_x + "," + gyro_y + "," + gyro_z + "\n");
-      saveSensorDataMagnitude(eSenseGyrosMag, gyro_x, gyro_y, gyro_z);
-      if (isStopped){c.stopNotifications();}
-    });
-    return c.startNotifications();
-  }).catch(function(){
-    console.log("Notification Error");
+  })
+  .then((device) => {
+    return device.gatt.connect({minInterval:20, maxInterval:40});
   });
 }
 
-function saveSensorDataMagnitude(file, x, y, z) {
-  let val = x*x + y*y + z*z;
-  let magnitude = Math.sqrt(val);
-  file.write(magnitude + "\n");
-  return magnitude;
+/**
+ * @param {BluetoothRemoteGATTServer} server 
+ */
+function bondWithESense(server) {
+  return server.startBonding()
+    .then(() => {
+      console.log("bonded: ", server.getSecurityStatus());
+    });
 }
 
 /**
- * Calculate value from most significant and least significant byte
+ * @param {BluetoothRemoteGATTServer} server 
+ * @returns {Promise<BluetoothRemoteGATTService>}
+ */
+function getBLEService(server) {
+  return server.getPrimaryService("0xFF06");
+}
+
+/**
+ * @param {BluetoothRemoteGATTService} service 
+ * @param {number} sampleRate 
+ * @returns {Promise<void>}
+ */
+function setSensorConfig(service, sampleRate) {
+  // IMU sensor config characterisic
+  return service
+    .getCharacteristic("0xFF07")
+    .then((c) => {
+      // Activate IMU sensor and set sampling rate
+      let checkSum = sampleRate + 2 + 1;
+      let setting = [83, checkSum, 2, 1, sampleRate];
+      return c.writeValue(setting);
+    });
+}
+
+
+/**
+ * @param {BluetoothRemoteGATTService} service 
+ * @returns {Promise<void>}
+ */
+function turnOffSensor(service) {
+  // IMU sensor config characterisic
+  return service
+    .getCharacteristic("0xFF07")
+    .then((c) => {
+      // Deactivate IMU sensor
+      let checkSum = 0 + 2 + 0;
+      let setting = [83, checkSum, 2, 0, 0];
+      return c.writeValue(setting);
+    });
+}
+
+
+/**
+ * @param {BluetoothRemoteGATTService} service 
+ * @returns {Promise<void>}
+ */
+function setSensorListener(service) {
+  // Characteristic to receive sensor data
+  return service
+    .getCharacteristic("0xFF08")
+    .then(
+      /** 
+       * @param {BluetoothRemoteGATTCharacteristic} c 
+       * @returns {Promise<void>}
+       */
+      function (c) {
+        // Set listener for eSense notifications
+        c.on('characteristicvaluechanged', function(event) {
+          let data = [].slice.call(event.target.value.buffer);
+          counter++;
+          console.log("eSense: " + data);
+          let timestamp = parseInt(Date.now());
+          // save Data to files
+          saveAccelData(data, timestamp);
+          saveGyrosData(data, timestamp);
+          // read Bangle Accel
+          saveBangleAccel(timestamp);
+          if (isStopped) {
+            c.stopNotifications();
+          }
+        });
+        return c.startNotifications();
+      }
+    );
+}
+
+// First menu
+const mainmenu = {
+  "" : {
+    title: " - Main Menu - ",
+    fontHeight: 20
+  },
+  "Connect to eSense" : () => {
+    E.showMessage("Connecting...");
+    connectToESense()
+      .then((server) => {
+        gatt = server;
+        console.log("Connected to eSense");
+        getBLEService(gatt)
+          .then((s) => {
+            service = s;
+            console.log("Found primary service");
+            isStopped = false;
+            E.showMenu(eSenseMenu);
+            setInterval(() => {
+              if (gatt.connected) {
+                console.log("Still connected");
+              } else {
+                console.log("Connection lost");
+                E.showAlert("Disconnected!")
+                  .then(() => {
+                    clearInterval();
+                    E.showMenu(mainmenu);
+                  });
+              }
+            }, 3000)
+          })
+          .catch((err) => {
+            console.log("Error: " + err);
+            console.log("Failed to get the service")
+            if (gatt.connected) {
+              gatt.disconnect()
+                .then(() => console.log("Automatically Disconnected"))
+                .catch(() => console.log("Failed to disconnect"));
+            }
+            E.showAlert("Connected, but failed to get the service").then(() => {
+              E.showMenu(mainmenu);
+            });
+          });
+      })
+      .catch((err) => {
+        console.log("Error: " + err);
+        console.log("Failed to connect");
+        E.showAlert("Failed to connect")
+          .then(() => {
+            E.showMenu(mainmenu);
+          });
+      });
+  },
+  "Exit" : () => { E.showMenu(mainmenu); },
+};
+
+// Submenu
+const eSenseMenu = {
+  "" : {
+    title : " - eSense connected - ",
+    fontHeight: 20,
+  },
+  "Sample Rate" : { 
+    value: sampleRate,
+    min:10,max:50,step:5,
+    onchange : (v) => {
+      setSensorConfig(service, v)
+        .then(() => {
+          sampleRate = v;
+          console.log("Set sampling rate: ", v);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  },
+  "Start Listener" : () => {
+    // Create files to save data
+    E.showMessage("Starting Listener...");
+    startedAt = parseInt(Date.now() / 1000);
+    bangleAccel = require("Storage").open("bangleAccel_" + startedAt + ".csv", "a");
+    eSenseGyros = require("Storage").open("eSenseGyros_" + startedAt + ".csv", "a");
+    eSenseAccel = require("Storage").open("eSenseAccel_" + startedAt + ".csv", "a");
+    setSensorConfig(service, sampleRate)
+      .then(() => {
+        setSensorListener(service)
+          .then(() => {
+            console.log("Started listener");
+            E.showMessage("Listener started");
+            //start Bangle Acclerometer
+            startAccel();
+            setWatch(() => {
+              eSenseMenu["Stop Listener"].call()
+            }, BTN1, {repeat: false});
+            setInterval(() => E.showMessage("Sample Count: " + counter), 3000)
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  "Stop Listener" : () => {
+    // Stop Bangle Accel
+    stopAccel();
+    isStopped = true;
+    clearInterval();
+
+    setTimeout(() => {
+      console.log("Stopped Listener");
+      E.showMessage("Stopped Listener");
+      eSenseAccel = undefined;
+      eSenseGyros = undefined;
+      bangleAccel = undefined;
+    }, 1000)
+    
+    // save Magnitudes
+    setTimeout(() => {
+      saveMagnitudes();
+      E.showMenu(eSenseMenu);
+    }, 2000);
+  },
+  "Remove last recording" : () => {
+    console.log("Removing files written at: " + startedAt);
+    cleanFiles(startedAt);
+  },
+  "Disconnect" : () => {
+    isStopped = true;
+    setTimeout(() => {
+      console.log("Stopped Listener");
+      turnOffAndDisconnect(service);
+    }, 1000);
+  },
+};
+
+
+function turnOffAndDisconnect(service) {
+  turnOffSensor(service)
+    .then(() => {
+      console.log("Turned off sensor");
+      disconnectESense();
+    })
+    .catch((err) => {
+      console.log(err);
+      disconnectESense();
+    });
+}
+
+function disconnectESense() {
+  if (gatt.connected) {
+    gatt.disconnect()
+      .then(() => console.log("Disconnected"))
+      .catch(() => console.warn("Disconnect failed"));
+  }
+  // Display
+  clearInterval();
+  E.showMenu(mainmenu);
+}
+
+E.showMenu(mainmenu);
+
+function cleanFiles(startedAt) {
+  require("Storage").open("eSenseGyros_" + startedAt + ".csv", "r").erase();
+  require("Storage").open("eSenseAccel_" + startedAt + ".csv", "r").erase();
+  require("Storage").open("bangleAccel_" + startedAt + ".csv", "r").erase();
+  require("Storage").open("eSenseGyMag_" + startedAt + ".csv", "r").erase();
+  require("Storage").open("eSenseAcMag_" + startedAt + ".csv", "r").erase();
+  require("Storage").open("bangleAcMag_" + startedAt + ".csv", "r").erase();
+}
+
+
+// ############### eSense Data Helper ###############
+
+function saveAccelData(data, timestamp) {
+  let accl_x = combineBytes(data[10], data[11]);
+  let accl_y = combineBytes(data[12], data[13]);
+  let accl_z = combineBytes(data[14], data[15]);
+  eSenseAccel.write("" + accl_x + "," + accl_y + "," + accl_z + "," + timestamp + "\n");
+}
+
+function saveGyrosData(data, timestamp) {
+  let gyro_x = combineBytes(data[4], data[5]);
+  let gyro_y = combineBytes(data[6], data[7]);
+  let gyro_z = combineBytes(data[8], data[9]);
+  eSenseGyros.write("" + gyro_x + "," + gyro_y + "," + gyro_z + "," + timestamp + "\n");
+}
+
+
+function saveMagnitudes() {
+  // Open files with sensor data in READ mode
+  // create new files to save the magnitudes to
+  setTimeout(() => {
+      saveMagnitude("bangleAccel_" + startedAt + ".csv", "bangleAcMag_" + startedAt + ".csv");
+  }, 1000)
+  setTimeout(() => {
+      saveMagnitude("eSenseGyros_" + startedAt + ".csv", "eSenseGyMag_" + startedAt + ".csv");
+  }, 1000)
+  setTimeout(() => {
+      saveMagnitude("eSenseAccel_" + startedAt + ".csv", "eSenseAcMag_" + startedAt + ".csv");
+  }, 1000)
+  // startedAt = undefined;
+}
+
+function saveMagnitude(filename, magFilename) {
+  let file = require("Storage").open(filename, "r");
+  let magFile = require("Storage").open(magFilename, "a");
+  extractMagnitude(file, magFile);
+}
+
+
+/** Read the data from one file and write their magnitude in the other
+ * 
+ * @param {StorageFile} file 
+ * @param {StorageFile} magFile 
+ */
+function extractMagnitude(file, magFile) {
+  let line = file.readLine();
+  while (line !== undefined) {
+    let data = line.split(",");
+    let magnitude = calculateMagnitude(data[0], data[1], data[2]);
+    magFile.write(magnitude + "\n");
+    line = file.readLine();
+  }
+}
+
+/** Calculate Magnitude from x, y and z value
+ * 
+ * @param {number} x 
+ * @param {number} y 
+ * @param {number} z
+ * @returns {number}
+ */
+function calculateMagnitude(x, y, z) {
+  let val = x*x + y*y + z*z;
+  let magnitude = Math.sqrt(val);
+  return magnitude;
+}
+
+/** Calculate value from most significant and least significant byte
+ * 
  * @param {number} msb 
  * @param {number} lsb 
  * @returns {number}
@@ -114,29 +381,12 @@ function combineBytes(msb, lsb) {
   return msblsb;
 }
 
-//Start the app by pressing BTN1
-setWatch(() => {
-  //On first button press, Bangle connects to eSense and starts sending data.
-  if (!isStarted) {
-    //cleanFiles();
-    connectToESense();
-    isStarted = true;
-    console.log("BTN 1 pressed: Start up");
-  }
-  //On second button press, the data transmission is stopped. 
-  else {
-    isStopped = true;
-    stopAccel();
-    console.log("BTN 1 pressed again: Stop notifications");
-  }
-}, BTN1, {
-  repeat: true
-});
+// ############### Bangle.js Acclerometer ##############
 
-function accelHandler(value) {
-  console.log( "Bangle: " + "x: " + value.x + " y: " + value.y + " z:" + value.z + " Mag: " + value.mag + " Diff: " + value.diff);
-  bangleAccel.write("" + value.x + "," + value.y + "," + value.z + "\n");
-  bangleAccelMagnitude.write(value.mag + "\n");
+function saveBangleAccel(timestamp) {
+  let data = Bangle.getAccel();
+  console.log( "Bangle: " + "x: " + data.x + " y: " + data.y + " z:" + data.z);
+  bangleAccel.write("" + data.x + "," + data.y + "," + data.z + "," + timestamp + "\n");
 }
 
 function accelConfig(hz,gs) {
@@ -154,16 +404,24 @@ function accelConfig(hz,gs) {
   Bangle.accelWr(0x18,0b11100100|gs); // CNTL1on,lowp,gs,TDTE,noWakeup,noTilt
 }
 
-
-function startUpAccel() {
-  let hz = 50;
-  let gs = 2;
+function startAccel() {
+  let hz = sampleRate;
+  let gs = 4;
   accelConfig(hz, gs);
-  Bangle.setPollInterval((1000/hz));
-  Bangle.on('accel',accelHandler);
+  //Bangle.setPollInterval((1000/hz));
+  //Bangle.on('accel', accelHandler);
 }
 
 function stopAccel() {
-  Bangle.on('accel', () => {});
-  Bangle.softOff();
+  Bangle.accelWr(0x18, 0x0A) // deactivate accel
+  //Bangle.softOff(); // reset()
+  // no good solution found
+}
+
+function accelHandler(value) {
+  console.log( "Bangle: " + "x: " + value.x + " y: " + value.y + " z:" + value.z + " Mag: " + value.mag + " Diff: " + value.diff);
+  //bangleAccel.write("" + value.x + "," + value.y + "," + value.z + "\n");
+  //bangleAcMag.write(value.mag + "\n");
+}
+
 }
